71e5e62514be84101c2a6cb2d778e6dce2093ee5
Diogo Muller <diogo.muller@live.com>
2014-04-18 16:37:09 -0300

    Added first assets to do initial tests.

--- src/BattlemageArena/Program.cs
+++ src/BattlemageArena/Program.cs
@@ -10,7 +10,7 @@ namespace BattlemageArena
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (GameMain game = new GameMain())
             {
                 game.Run();
             }
