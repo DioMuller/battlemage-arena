5386a3b0033b5fe06fd18dde21277b4215055bfc
Diogo Muller <diogo.muller@live.com>
2014-05-09 21:59:38 -0300

    Base code for lan multiplayer.

--- src/BattlemageArena/GameLogic/Net/NetworkConnection.cs
+++ src/BattlemageArena/GameLogic/Net/NetworkConnection.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Net;
 
 namespace BattlemageArena.GameLogic.Net
 {
     class NetworkConnection
     {
+        private static NetworkSession _session;
+
         public static bool SignIn()
         {
             if (Gamer.SignedInGamers.Count == 0)
@@ -26,5 +29,51 @@ namespace BattlemageArena.GameLogic.Net
                 return true;
             }
         }
+
+        public static void CreateSession()
+        {
+            _session = NetworkSession.Create(NetworkSessionType.SystemLink, 1, 2);
+            _session.GamerJoined += new EventHandler<GamerJoinedEventArgs>(Host_GamerJoined);
+            _session.GamerLeft += new EventHandler<GamerLeftEventArgs>(Host_GamerLeft); 
+            
+            // TODO: Change game state to WAIT FOR PLAYERS
+        }
+
+        public static void SearchForGame()
+        {
+            AvailableNetworkSessionCollection sessions = NetworkSession.Find(NetworkSessionType.SystemLink, 1, null);
+            if (sessions.Count > 0)
+            {
+                AvailableNetworkSession mySession = sessions[0]; 
+                _session = NetworkSession.Join(mySession); 
+                _session.GamerLeft += new EventHandler<GamerLeftEventArgs>(Client_GamerLeft); 
+                //StartGame(); 
+                //_gameState = GameState.PlayingAsPlayer;
+            } 
+        }
+
+        #region Event Handlers
+
+        static void Host_GamerJoined(object sender, GamerJoinedEventArgs e)
+        {
+            if (_session.RemoteGamers.Count <= 1)
+            {
+                //StartGame(); 
+                //state = GameState.PlayingAsHost;
+            }
+        }
+
+        static void Host_GamerLeft(object sender, GamerLeftEventArgs e)
+        {
+            _session.Dispose(); 
+            //EndGame();
+        }
+
+        static void Client_GamerLeft(object sender, GamerLeftEventArgs e)
+        {
+            _session.Dispose(); 
+            //EndGame();
+        } 
+        #endregion Event Handlers
     }
 }
